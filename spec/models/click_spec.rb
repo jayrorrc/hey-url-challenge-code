4934eef044728f43d5a302d8d850e0f2be939ee9
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2021-01-31 10:32:15 -0300

    Create test for click model

--- spec/models/click_spec.rb
+++ spec/models/click_spec.rb
@@ -4,16 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Click, type: :model do
   describe 'validations' do
-    it 'validates url_id is valid' do
-      skip 'add test'
-    end
-
-    it 'validates browser is not null' do
-      skip 'add test'
-    end
-
-    it 'validates platform is not null' do
-      skip 'add test'
-    end
+    it { should belong_to(:url) }
+    it { should validate_presence_of(:browser) }
+    it { should validate_presence_of(:platform) }
   end
 end
