79c4452a87823738a372f9aa200ed5af1c8186b7
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2021-01-31 10:33:39 -0300

    Implement url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class Url < ApplicationRecord
-  # scope :latest, -> {}
+  has_many :clicks
+
+  validates :original_url, :short_url, :clicks_count, presence: true
+  validates :original_url, :short_url, uniqueness: true
+  validates_format_of :original_url, { with: URI.regexp, message: "This is not a valid url"}
+  validates_length_of :short_url, maximum: 5
+  validates :short_url, format: { with: /\A[a-zA-Z0-9]+\z/ }
+
+  def clicks_count
+    clicks.where("created_at >= ? AND created_at <= ?", Time.now.beginning_of_month, Time.now.end_of_month).length
+  end
 end
